c9b758ea0c398df6d258f15d502ba3d24796e455
Andy <andyjhorn@gmail.com>
2020-12-26 15:08:32 -0600

    installed firebase and dependencies

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -30,7 +30,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: kLogoHeroTag,
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: 50.0,
                   ),
                 ),
                 TypewriterAnimatedTextKit(
